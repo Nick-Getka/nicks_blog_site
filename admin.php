620a44cc8a321e9e766308528f58411bd82fef86
Nick-Getka <ngetka@ngetka.localdomain>
2017-01-19 13:37:00 -0500

    Added Login Test

--- admin.php
+++ admin.php
@@ -39,8 +39,9 @@ function login(){
   if(isset( $_POST['login'])){
     if($_POST['username'] == ADMIN_USERNAME && $_POST['password'] == ADMIN_PASSWORD){
       $_SESSION['username'] == ADMIN_USERNAME;
-      print("SUCCESS!");
-      header( "Location: admin.php" );
+      require( TEMPLATE_PATH . "/cs_temp.php" );
+      // print("SUCCESS!");
+      // header( "Location: admin.php" );
     }
     else{
       print("FAILURE!");
